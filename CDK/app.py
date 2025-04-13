9e130054f1d312566d34cd5feb3b7a90ace8fde4
Laura <laura.nagamine@utec.edu.pe>
2025-04-13 10:57:58 -0500

    cambios 2

--- CDK/app.py
+++ CDK/app.py
@@ -8,7 +8,13 @@ from cdk.cdk_stack import CdkStack
 
 app = cdk.App()
 CdkStack(app, "CdkStack",
-     env=cdk.Environment(account="619874379465", region="us-east-1")
+     env=cdk.Environment(account="619874379465", region="us-east-1"),
+    synthesizer=cdk.DefaultStackSynthesizer(
+        qualifier="labenv",
+        # Use existing execution role
+        deploy_role_arn="arn:aws:iam::619874379465:role/LabRole",
+        cloud_formation_execution_role="arn:aws:iam::619874379465:role/LabRole"
     )
+)
 
 app.synth()
