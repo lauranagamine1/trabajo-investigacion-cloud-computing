c8b77c5e911e07e4def802a7192b365e1894cc6d
Laura <laura.nagamine@utec.edu.pe>
2025-04-13 11:00:33 -0500

    cambios 3

--- CDK/app.py
+++ CDK/app.py
@@ -9,11 +9,15 @@ from cdk.cdk_stack import CdkStack
 app = cdk.App()
 CdkStack(app, "CdkStack",
      env=cdk.Environment(account="619874379465", region="us-east-1"),
+    # Use custom synthesizer with minimal requirements
     synthesizer=cdk.DefaultStackSynthesizer(
         qualifier="labenv",
-        # Use existing execution role
+        file_assets_bucket_name="cdk-hnb659fds-assets-619874379465-us-east-1",
         deploy_role_arn="arn:aws:iam::619874379465:role/LabRole",
-        cloud_formation_execution_role="arn:aws:iam::619874379465:role/LabRole"
+        cloud_formation_execution_role="arn:aws:iam::619874379465:role/LabRole",
+        lookup_role_arn="arn:aws:iam::619874379465:role/LabRole",
+        file_assets_publishing_role_arn="arn:aws:iam::619874379465:role/LabRole",
+        image_assets_publishing_role_arn="arn:aws:iam::619874379465:role/LabRole"
     )
 )
 
