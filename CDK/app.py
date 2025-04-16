5a4ddb2d0b47801f3038f51309db36beb529a2f3
Laura <laura.nagamine@utec.edu.pe>
2025-04-15 19:22:09 -0500

    cambiando

--- CDK/app.py
+++ CDK/app.py
@@ -1,18 +1,3 @@
-import os
-
-import aws_cdk as cdk
-
-from cdk.cdk_stack import CdkStack
-
-
-app = cdk.App()
-
-CdkStack(app, "CdkStack",
-     env=cdk.Environment(account="619874379465", region="us-east-1")
-)
-
-app.synth()
-
 from aws_cdk import App, Environment, DefaultStackSynthesizer
 from cdk_python.cdk_python_stack import PilaEc2
 import boto3
